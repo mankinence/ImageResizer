a70377a8b0f9a732fd4a74de758ee0172bc47cb1
Searene <mycityofsky@gmail.com>
2022-06-07 22:10:56 +0800

    move PyQt5 import logic to another file

--- 2.1/__init__.py
+++ 2.1/__init__.py
@@ -14,24 +14,7 @@ from aqt import mw
 from aqt.editor import Editor, EditorWebView
 from aqt.qt import *
 from bs4 import BeautifulSoup
-
-try:
-    from PyQt6 import QtCore, QtGui, QtWidgets
-    from PyQt6.QtGui import QAction, QImage
-    from PyQt6.QtCore import Qt, QMimeData
-    from PyQt6.QtWidgets import (
-        QMainWindow, QApplication,
-        QLabel, QToolBar, QStatusBar, QWidget, QVBoxLayout, QMessageBox, QCheckBox, QPushButton, QComboBox, QHBoxLayout,
-        QLineEdit
-    )
-except ImportError:
-    from PyQt5 import QtGui
-    from PyQt5.QtCore import Qt, QMimeData
-    from PyQt5.QtWidgets import (
-        QApplication,
-        QLabel, QVBoxLayout, QMessageBox, QCheckBox, QPushButton, QComboBox, QHBoxLayout,
-        QLineEdit
-    )
+from .resizer.PyQt import *
 
 addon_id = '1214357311'
 
--- 2.1/deploy_locally.cmd
+++
@@ -1,2 +0,0 @@
-set target_dir=%homedrive%%homepath%\AppData\Roaming\Anki2\addons21\1214357311
-copy __init__.py %target_dir%
